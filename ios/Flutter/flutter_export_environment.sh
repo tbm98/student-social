eb013ac212f97e652ea32005ee2b8385dcebb8e7
tbm98 <tbm98.dev@gmail.com>
2019-11-11 22:38:12 +0700

    init for testing

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/admin/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/admin/Documents/Project_flutter/student-social"
-export "FLUTTER_TARGET=/Users/admin/Documents/Project_flutter/student-social/lib/main.dart"
+export "FLUTTER_ROOT=/home/tbm98/Documents/FLUTTER_SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/tbm98/Documents/mobile_projects/student-social"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/admin/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/tbm98/Documents/FLUTTER_SDK/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
