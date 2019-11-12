8415cf3644cf420046cd711740c2ee38d5c14779
tbm98 <tbm98.dev@gmail.com>
2019-11-12 10:10:17 +0700

    add testcase for 'tao qrcode'

---
+++ test_driver/qrcode/tc1.dart
@@ -0,0 +1,40 @@
+// Imports the Flutter Driver API.
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('tao qrcode', () {
+
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    tap(SerializableFinder f) async{
+      return driver.tap(f);
+    }
+
+    wait(SerializableFinder f) async{
+      return driver.waitFor(f);
+    }
+
+    test('tc1', () async {
+      // Use the `driver.getText` method to verify the counter starts at 0.
+      await tap(find.byTooltip('Open navigation menu'));
+      await tap(find.byValueKey('taoqrcode'));
+      await tap(find.byValueKey('inputqrcode'));
+      await driver.enterText('DTC16HDXX');
+      await tap(find.byValueKey('qrtao'));
+      expect(await driver.getText(find.byValueKey('qrketqua')), 'DTC16HDXX');
+    });
+  });
+}
